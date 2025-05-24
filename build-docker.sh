e3ce478f93bc064435f4717f0ace632a6f4186be
pdaxrom <sashz@pdaxrom.org>
2025-05-24 15:51:32 +0400

    o32 and n32 are split into two docker images

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,8 @@
 
 set -ex
 
-docker build --tag 'irix-cross' .
+docker build --tag 'irix-cross' -f Dockerfile .
+docker build --tag 'irix-cross-n32' -f Dockerfile.n32 .
 
 docker run -ti --rm -v .:/out irix-cross /bin/bash /root/build-irix-gcc.sh
-docker run -ti --rm -v .:/out irix-cross /bin/bash /root/build-irix-gcc.sh config-irix65.inc
+docker run -ti --rm -v .:/out irix-cross-n32 /bin/bash /root/build-irix-gcc.sh config-irix65.inc
